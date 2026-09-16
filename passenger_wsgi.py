f64eadf97ed958db96274b38bd71f7275d270963
sudip6164 <sudippradhanadgj@gmail.com>
2026-09-16 19:17:13 +0545

    feat(wsgi): refactor WSGI application setup for improved clarity and structure

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,10 @@
 import os
-from config.wsgi import application
+import sys
 
-app = application
+sys.path.insert(0, os.path.dirname(__file__))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
